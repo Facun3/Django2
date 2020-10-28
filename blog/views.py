cbd41abd88dbabddb638cfdf90f82b5e2fa51ee4
Facundo <facundo.choque.silberman@itscordoba.edu.ar>
2020-10-28 15:36:28 -0300

    added Post from .models

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
